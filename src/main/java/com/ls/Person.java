72055a1e762e23b8cfbd1e9744fdae03a429ba6c
Laiba Sajid <laibasajid@Laibas-MBP.lan>
2024-10-23 19:20:33 -0400

    Person class passing zybooks test

--- src/main/java/com/ls/Person.java
+++ src/main/java/com/ls/Person.java
@@ -15,16 +15,16 @@ public class Person {
         this.birthYear = birthYear;
     }
 
-    public String getName(String name) {
+    public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public int getBirthYear() {
         return birthYear;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public void setBirthYear(int birthYear) {
@@ -32,16 +32,44 @@ public class Person {
     }
 
     @Override
-    public boolean equals(Object obj) {
+    public boolean equals(Object object) {
+        if (object == null) {
+            return false;
+        }
+
+        if (this == object) {
+            return true;
+        }
+
+        if (object instanceof Person) {
+            Person otherPerson = (Person) object;
+             if ((this.name == otherPerson.name) && (this.birthYear == otherPerson.birthYear)) {
+                 return true;
+             }
+        }
         return false;
     }
 
     @Override
     public String toString () {
-        return "";
+        String s = "";
+        s = String.format(
+                "Person: Name: %30s | Birth Year: %4d",
+                name,
+                birthYear
+        );
+        return s;
     }
 
-    public int compareTo(Person p) {
-        return -1;
+
+    public int compareTo(Person otherPerson) {
+        if (otherPerson == null) {return 0;}
+
+        if (this.birthYear < otherPerson.birthYear) {
+            return -1;
+        } else if (this.birthYear > otherPerson.birthYear) {
+            return 1;
+        }
+        return 0;
     }
 }
