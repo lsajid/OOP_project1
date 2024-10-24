03868fd1de9846d9562ecf8ff6da7cc6155f3f84
Laiba Sajid <laibasajid@Laibas-MBP.lehman.edu>
2024-10-24 12:28:23 -0400

    fix: try to debug .compareTo function

--- src/main/java/com/ls/Student.java
+++ src/main/java/com/ls/Student.java
@@ -1,30 +1,46 @@
-package com.ls;
+//package com.ls;
+
+import java.util.Objects;
 
 public class Student extends Person {
-    private static int numStudents;
+    private static int numStudents = 0;
     private int studentID;
     private Course[] coursesTaken;
     private int numCoursesTaken;
     private boolean isGraduate;
-    private String major;
+    private String major = "undeclared";
 
     public Student() {
-        this.coursesTaken = new Course[0];
+        this.coursesTaken = new Course[50];
         this.numCoursesTaken = 0;
         this.isGraduate = false;
+        this.studentID = ++numStudents;
     }
 
     public Student(boolean isGraduate) {
         this.isGraduate = isGraduate;
+        this.studentID = ++numStudents;
     }
 
     public Student(String major, boolean isGraduate) {
         this.major = major;
         this.isGraduate = isGraduate;
+        this.studentID = ++numStudents;
+    }
+
+    public Student(String name, int birthYear, String major, boolean isGraduate) {
+        super(name, birthYear);
+        this.major = major;
+        this.isGraduate = isGraduate;
+        this.studentID = ++numStudents;
     }
 
     public boolean isGraduate() {
         return isGraduate;
+    }
+
+    public int getNumCoursesTaken() {
+        return numCoursesTaken;
     }
 
     public static int getNumStudents() {
@@ -45,6 +61,80 @@ public class Student extends Person {
 
     public void setMajor(String major) {
         this.major = major;
+    }
+
+    public static boolean validateIndex(Course[] arr, int index) {
+        if (index < 0) return false;
+        if (index > arr.length) return false;
+        return true;
+    }
+
+    public void addCourseTaken(Course course) {
+        for (int i=0; i < coursesTaken.length; i++) {
+            if (coursesTaken[i] == null) {
+                coursesTaken[i] = course;
+                break;
+            }
+        }
+        numCoursesTaken++;
+    }
+
+    public void addCoursesTaken(Course[] courses) {
+        for (int i=0; i < courses.length; i++) {
+            addCourseTaken(courses[i]);
+        }
+    }
+
+    public Course getCourseTaken(int index) {
+        if(!validateIndex(coursesTaken,index)) return null;
+        for (int i = 0; i < coursesTaken.length; i++) {
+            if(i == index) {
+                return coursesTaken[i];
+            }
+        }
+        return null;
+    }
+
+    public String getCourseTakenAsString(int index) {
+        Course course = getCourseTaken(index);
+        return course.toString();
+    }
+
+    public String getAllCoursesTakenAsString() {
+        String s = "";
+        if (coursesTaken == null) return "";
+        for(int i = 0; i < coursesTaken.length; i++) {
+            if (coursesTaken[i] == null) break;
+            s += getCourseTakenAsString(i);
+        }
+        return s;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null || getClass() != object.getClass()) return false;
+        if (this == object) return true;
+
+        if (super.equals(object)) {
+            Student otherStudent = (Student) object;
+            if (
+                    studentID == otherStudent.studentID &&
+                    numCoursesTaken == otherStudent.numCoursesTaken &&
+                            isGraduate == otherStudent.isGraduate &&
+                            Objects.deepEquals(coursesTaken, otherStudent.coursesTaken) &&
+                            Objects.equals(major, otherStudent.major)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        String graduate = isGraduate ? "Graduate" : "Undergraduate";
+        String studentIDFormat = String.format(" Student: studentID: %04d", studentID);
+        return super.toString() + studentIDFormat +" | Major           " + major + " |       " + graduate + " | Number of Courses Taken:   " + numCoursesTaken + " | Courses Taken: " + getAllCoursesTakenAsString();
     }
 
 }
